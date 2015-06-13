7229c7d4322acbde5a2bbd472b6bdfb744778386
tcosta <vinicius.remigio@gmail.com>
2015-06-13 16:10:34 -0300

    fluxo de templates

--- src/main/java/br/com/elo7/selecao/controller/RoboController.java
+++ src/main/java/br/com/elo7/selecao/controller/RoboController.java
@@ -2,15 +2,46 @@ package br.com.elo7.selecao.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.elo7.selecao.business.Mudar;
+import br.com.elo7.selecao.modelo.Coordenada;
+import br.com.elo7.selecao.modelo.Direcao;
+import br.com.elo7.selecao.modelo.Movimento;
+import br.com.elo7.selecao.modelo.Robo;
 
 @Controller
 public class RoboController {
-	
 
-	    @RequestMapping("/")
-	    public String index() {
-	        return "nasa";
-	    }
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String inicializarForms() {
+		return "nasa";
+	}
 
+	@RequestMapping(value = "/nasaForm", method = RequestMethod.POST)
+	public ModelAndView getParametrosEntrada(
+			@RequestParam("tamanho_planalto") String planalto,
+			@RequestParam("coordenadas_robot") String coordenadas_robot,
+			@RequestParam("direcao_robot") String direcao_robot,
+			@RequestParam("caminho_robot") String caminho_robot) {
 
+		String coordPlanalto[] = planalto.split(",");
+		String coordRobot[] = coordenadas_robot.split(",");
+		Direcao direcaoRobot = Direcao.valueOf(direcao_robot);
+		String caminhos = caminho_robot;
+
+		Robo robo = new Robo(new Coordenada(Integer.valueOf(coordRobot[0]),
+				Integer.valueOf(coordRobot[1])), direcaoRobot);
+
+		Mudar mudar = new Mudar();
+
+		mudar.mudarEstado(robo, Movimento.MOVE);
+
+		ModelAndView mav = new ModelAndView("result");
+		mav.addObject("saida", robo);
+
+		return mav;
+	}
 }
