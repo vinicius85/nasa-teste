5991a26f9547df8da6d13a38aa71a8e38c335139
tcosta <vinicius.remigio@gmail.com>
2015-06-13 15:12:44 -0300

    incluindo biblioteca do velocity e criando o template vm

---
+++ src/main/java/br/com/elo7/selecao/controller/MainApplication.java
@@ -0,0 +1,13 @@
+package br.com.elo7.selecao.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
--- src/main/java/br/com/elo7/selecao/controller/RoboController.java
+++ src/main/java/br/com/elo7/selecao/controller/RoboController.java
@@ -1,15 +1,15 @@
 package br.com.elo7.selecao.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class RoboController {
 	
 
 	    @RequestMapping("/")
 	    public String index() {
-	        return "Greetings from Spring Boot!";
+	        return "nasa";
 	    }
 
 
